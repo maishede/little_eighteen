0732813796b973f0330423c7026b133f0b10a2cd
unknown <zoulinjie@seceva.cn>
2025-11-26 01:17:03 +0800

    调整方向键

--- app/services/core.py
+++ app/services/core.py
@@ -6,7 +6,8 @@ from app.config import (
     IN1, IN2, IN3, IN4, IN5, IN6, IN7, IN8,
     HC_SR_04_TRIG, HC_SR_04_ECHO,
     MODE_FORWARD, MODE_BACK, MODE_STOP,
-    DEFAULT_TEMPERATURE, DISTANCE_BUFFER_SIZE, HC_SR_04_TIMEOUT
+    DEFAULT_TEMPERATURE, DISTANCE_BUFFER_SIZE, HC_SR_04_TIMEOUT,
+    EN1, EN2, EN3, EN4
 )
 
 # GPIO 常量
@@ -24,6 +25,7 @@ class MotorControl(object):
         for pin in [IN1, IN2, IN3, IN4, IN5, IN6, IN7, IN8]:
             GPIO.setup(pin, OUT)
             GPIO.output(pin, LOW)
+
         # 设置超声波传感器引脚
         GPIO.setup(HC_SR_04_TRIG, OUT)
         GPIO.setup(HC_SR_04_ECHO, IN)
@@ -66,18 +68,18 @@ class MotorControl(object):
 
         if mode == MODE_FORWARD:
             if is_right_motor:  # 如果是右侧电机，前进时实际需要反转信号
-                GPIO.output(motor_in1, LOW)
-                GPIO.output(motor_in2, HIGH)
-            else:  # 左侧电机，正常前进
                 GPIO.output(motor_in1, HIGH)
                 GPIO.output(motor_in2, LOW)
+            else:  # 左侧电机，正常前进
+                GPIO.output(motor_in1, LOW)
+                GPIO.output(motor_in2, HIGH)
         elif mode == MODE_BACK:
             if is_right_motor:  # 如果是右侧电机，后退时实际需要反转信号
-                GPIO.output(motor_in1, HIGH)
-                GPIO.output(motor_in2, LOW)
-            else:  # 左侧电机，正常后退
                 GPIO.output(motor_in1, LOW)
                 GPIO.output(motor_in2, HIGH)
+            else:  # 左侧电机，正常后退
+                GPIO.output(motor_in1, HIGH)
+                GPIO.output(motor_in2, LOW)
         elif mode == MODE_STOP:
             GPIO.output(motor_in1, LOW)
             GPIO.output(motor_in2, LOW)
@@ -88,40 +90,20 @@ class MotorControl(object):
     # --- 单独电机控制方法 (用于调试) ---
     def control_left_front(self, mode):
         """单独控制左前电机"""
+        self.__set_motor_state(IN7, IN8, mode)
+
+    def control_right_front(self, mode):
         self.__set_motor_state(IN1, IN2, mode)
 
-    # def control_left_back(self, mode):
-    #     """单独控制左后电机"""
-    #     self.__set_motor_state(IN3, IN4, mode)
-    #
-    # def control_right_front(self, mode):
-    #     """单独控制右前电机"""
-    #     self.__set_motor_state(IN5, IN6, mode)
-    #
-    # def control_right_back(self, mode):
-    #     """单独控制右后电机"""
-    #     self.__set_motor_state(IN7, IN8, mode)
-    #
-    # # 麦克纳姆轮的四轮控制私有方法 (保持不变)
-    # def __left_front_motor(self, mode=MODE_FORWARD):
-    #     self.control_left_front(mode)
-    #
-    # def __left_back_motor(self, mode=MODE_FORWARD):
-    #     self.control_left_back(mode)
-    #
-    # def __right_front_motor(self, mode=MODE_FORWARD):
-    #     self.control_right_front(mode)
-    #
-    # def __right_back_motor(self, mode=MODE_FORWARD):
-    #     self.control_right_back(mode)
+    def control_left_back(self, mode):
+        self.__set_motor_state(IN5, IN6, mode)
+
+    def control_right_back(self, mode):
+        self.__set_motor_state(IN3, IN4, mode)
 
     # --- 公共运动方法 (麦克纳姆轮运动逻辑) ---
     def move_forward(self):
         """小车向前直行"""
-        # self.__left_front_motor(MODE_FORWARD)
-        # self.__left_back_motor(MODE_FORWARD)
-        # self.__right_front_motor(MODE_FORWARD)
-        # self.__right_back_motor(MODE_FORWARD)
         self._control_motor_pair(IN1, IN2, MODE_FORWARD)
         self._control_motor_pair(IN3, IN4, MODE_FORWARD)
         self._control_motor_pair(IN5, IN6, MODE_FORWARD)
@@ -129,10 +111,6 @@ class MotorControl(object):
 
     def move_back(self):
         """小车向后直行"""
-        # self.__left_front_motor(MODE_BACK)
-        # self.__left_back_motor(MODE_BACK)
-        # self.__right_front_motor(MODE_BACK)
-        # self.__right_back_motor(MODE_BACK)
         self._control_motor_pair(IN1, IN2, MODE_BACK)
         self._control_motor_pair(IN3, IN4, MODE_BACK)
         self._control_motor_pair(IN5, IN6, MODE_BACK)
@@ -146,12 +124,8 @@ class MotorControl(object):
         右前轮：前进
         右后轮：后退
         """
-        # self.__left_front_motor(MODE_BACK)
-        # self.__left_back_motor(MODE_FORWARD)
-        # self.__right_front_motor(MODE_FORWARD)
-        # self.__right_back_motor(MODE_BACK)
-        self._control_motor_pair(IN1, IN2, MODE_BACK)
-        self._control_motor_pair(IN3, IN4, MODE_FORWARD)
+        self._control_motor_pair(IN1, IN2, MODE_FORWARD)
+        self._control_motor_pair(IN3, IN4, MODE_BACK)
         self._control_motor_pair(IN5, IN6, MODE_FORWARD)
         self._control_motor_pair(IN7, IN8, MODE_BACK)
 
@@ -163,12 +137,8 @@ class MotorControl(object):
         右前轮：后退
         右后轮：前进
         """
-        # self.__left_front_motor(MODE_FORWARD)
-        # self.__left_back_motor(MODE_BACK)
-        # self.__right_front_motor(MODE_BACK)
-        # self.__right_back_motor(MODE_FORWARD)
-        self._control_motor_pair(IN1, IN2, MODE_FORWARD)
-        self._control_motor_pair(IN3, IN4, MODE_BACK)
+        self._control_motor_pair(IN1, IN2, MODE_BACK)
+        self._control_motor_pair(IN3, IN4, MODE_FORWARD)
         self._control_motor_pair(IN5, IN6, MODE_BACK)
         self._control_motor_pair(IN7, IN8, MODE_FORWARD)
 
@@ -178,14 +148,10 @@ class MotorControl(object):
         左侧所有轮子：后退
         右侧所有轮子：前进
         """
-        # self.__left_front_motor(MODE_BACK)
-        # self.__left_back_motor(MODE_BACK)
-        # self.__right_front_motor(MODE_FORWARD)
-        # self.__right_back_motor(MODE_FORWARD)
-        self._control_motor_pair(IN1, IN2, MODE_BACK)
-        self._control_motor_pair(IN3, IN4, MODE_BACK)
-        self._control_motor_pair(IN5, IN6, MODE_FORWARD)
-        self._control_motor_pair(IN7, IN8, MODE_FORWARD)
+        self._control_motor_pair(IN1, IN2, MODE_FORWARD)
+        self._control_motor_pair(IN3, IN4, MODE_FORWARD)
+        self._control_motor_pair(IN5, IN6, MODE_BACK)
+        self._control_motor_pair(IN7, IN8, MODE_BACK)
 
     def turn_right(self):  # 原地右转
         """
@@ -193,55 +159,35 @@ class MotorControl(object):
         左侧所有轮子：前进
         右侧所有轮子：后退
         """
-        # self.__left_front_motor(MODE_FORWARD)
-        # self.__left_back_motor(MODE_FORWARD)
-        # self.__right_front_motor(MODE_BACK)
-        # self.__right_back_motor(MODE_BACK)
-        self._control_motor_pair(IN1, IN2, MODE_FORWARD)
-        self._control_motor_pair(IN3, IN4, MODE_FORWARD)
-        self._control_motor_pair(IN5, IN6, MODE_BACK)
-        self._control_motor_pair(IN7, IN8, MODE_BACK)
+        self._control_motor_pair(IN1, IN2, MODE_BACK)
+        self._control_motor_pair(IN3, IN4, MODE_BACK)
+        self._control_motor_pair(IN5, IN6, MODE_FORWARD)
+        self._control_motor_pair(IN7, IN8, MODE_FORWARD)
 
     # --- 斜向移动 (修正后的麦克纳姆轮逻辑，基于所有轮子方向校正) ---
     def move_left_forward(self):  # 左前斜向
-        # self.__left_front_motor(MODE_STOP)
-        # self.__left_back_motor(MODE_FORWARD)
-        # self.__right_front_motor(MODE_FORWARD)
-        # self.__right_back_motor(MODE_STOP)
         self._control_motor_pair(IN1, IN2, MODE_STOP)
         self._control_motor_pair(IN3, IN4, MODE_FORWARD)
-        self._control_motor_pair(IN5, IN6, MODE_FORWARD)
-        self._control_motor_pair(IN7, IN8, MODE_STOP)
-
-    def move_right_forward(self):  # 右前斜向
-        # self.__left_front_motor(MODE_FORWARD)
-        # self.__left_back_motor(MODE_STOP)
-        # self.__right_front_motor(MODE_STOP)
-        # self.__right_back_motor(MODE_FORWARD)
-        self._control_motor_pair(IN1, IN2, MODE_FORWARD)
-        self._control_motor_pair(IN3, IN4, MODE_STOP)
-        self._control_motor_pair(IN5, IN6, MODE_STOP)
-        self._control_motor_pair(IN7, IN8, MODE_FORWARD)
-
-    def move_left_back(self):  # 左后斜向
-        # self.__left_front_motor(MODE_BACK)
-        # self.__left_back_motor(MODE_STOP)
-        # self.__right_front_motor(MODE_STOP)
-        # self.__right_back_motor(MODE_BACK)
-        self._control_motor_pair(IN1, IN2, MODE_BACK)
-        self._control_motor_pair(IN3, IN4, MODE_STOP)
         self._control_motor_pair(IN5, IN6, MODE_STOP)
         self._control_motor_pair(IN7, IN8, MODE_BACK)
 
-    def move_right_back(self):  # 右后斜向
-        # self.__left_front_motor(MODE_STOP)
-        # self.__left_back_motor(MODE_BACK)
-        # self.__right_front_motor(MODE_BACK)
-        # self.__right_back_motor(MODE_STOP)
-        self._control_motor_pair(IN1, IN2, MODE_STOP)
-        self._control_motor_pair(IN3, IN4, MODE_BACK)
+    def move_right_forward(self):  # 右前斜向
+        self._control_motor_pair(IN1, IN2, MODE_BACK)
+        self._control_motor_pair(IN3, IN4, MODE_STOP)
+        self._control_motor_pair(IN5, IN6, MODE_FORWARD)
+        self._control_motor_pair(IN7, IN8, MODE_STOP)
+
+    def move_left_back(self):  # 左后斜向
+        self._control_motor_pair(IN1, IN2, MODE_FORWARD)
+        self._control_motor_pair(IN3, IN4, MODE_STOP)
         self._control_motor_pair(IN5, IN6, MODE_BACK)
         self._control_motor_pair(IN7, IN8, MODE_STOP)
+
+    def move_right_back(self):  # 右后斜向
+        self._control_motor_pair(IN1, IN2, MODE_STOP)
+        self._control_motor_pair(IN3, IN4, MODE_STOP)
+        self._control_motor_pair(IN5, IN6, MODE_STOP)
+        self._control_motor_pair(IN7, IN8, MODE_FORWARD)
 
     def stop(self):
         """停止所有电机，将所有控制引脚置低"""
@@ -316,8 +262,8 @@ if __name__ == '__main__':
         mc.stop()  # 停止所有电机，确保干净状态
         time.sleep(0.5)
 
-        print("调试左前电机：后退 1秒")
-        mc.control_left_front(MODE_BACK)
+        print("调试右前电机：前进 1秒")
+        mc.control_right_front(MODE_FORWARD)
         time.sleep(1)
         mc.stop()
         time.sleep(0.5)
@@ -328,107 +274,82 @@ if __name__ == '__main__':
         mc.stop()
         time.sleep(0.5)
 
-        print("调试左后电机：后退 1秒")
-        mc.control_left_back(MODE_BACK)
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("调试右前电机：前进 1秒")
-        mc.control_right_front(MODE_FORWARD)
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("调试右前电机：后退 1秒")
-        mc.control_right_front(MODE_BACK)
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
         print("调试右后电机：前进 1秒")
         mc.control_right_back(MODE_FORWARD)
         time.sleep(1)
         mc.stop()
         time.sleep(0.5)
 
-        print("调试右后电机：后退 1秒")
-        mc.control_right_back(MODE_BACK)
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-        print("--- 单个电机调试结束 ---")
-
-        # 以下是原来的运动测试示例，您可以根据需要保留或删除
-        print("\n--- 开始麦克纳姆轮运动测试 ---")
-        print("前进")
-        mc.move_forward()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("后退")
-        mc.move_back()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("左平移")
-        mc.move_left()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("右平移")
-        mc.move_right()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("原地左转")
-        mc.turn_left()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("原地右转")
-        mc.turn_right()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("左前斜向")
-        mc.move_left_forward()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("右前斜向")
-        mc.move_right_forward()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("左后斜向")
-        mc.move_left_back()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("右后斜向")
-        mc.move_right_back()
-        time.sleep(1)
-        mc.stop()
-        time.sleep(0.5)
-
-        print("测试距离检测 (5秒)")
-        mc.distance_detection_enabled = True
-        for _ in range(50):  # 测量5秒，每0.1秒一次
-            dist = mc.measure_distance()
-            if dist is not None and dist != -1:  # 确保不是None和-1
-                print(f"Distance: {dist} cm")
-            time.sleep(0.1)
-        mc.distance_detection_enabled = False  # 禁用检测
-        print("--- 麦克纳姆轮运动测试结束 ---")
+        # # 以下是原来的运动测试示例，您可以根据需要保留或删除
+        # print("\n--- 开始麦克纳姆轮运动测试 ---")
+        # print("前进")
+        # mc.move_forward()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("后退")
+        # mc.move_back()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("左平移")
+        # mc.move_left()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("右平移")
+        # mc.move_right()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("原地左转")
+        # mc.turn_left()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("原地右转")
+        # mc.turn_right()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("左前斜向")
+        # mc.move_left_forward()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("右前斜向")
+        # mc.move_right_forward()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("左后斜向")
+        # mc.move_left_back()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("右后斜向")
+        # mc.move_right_back()
+        # time.sleep(1)
+        # mc.stop()
+        # time.sleep(0.5)
+        #
+        # print("测试距离检测 (5秒)")
+        # mc.distance_detection_enabled = True
+        # for _ in range(50):  # 测量5秒，每0.1秒一次
+        #     dist = mc.measure_distance()
+        #     if dist is not None and dist != -1:  # 确保不是None和-1
+        #         print(f"Distance: {dist} cm")
+        #     time.sleep(0.1)
+        # mc.distance_detection_enabled = False  # 禁用检测
+        # print("--- 麦克纳姆轮运动测试结束 ---")
 
     print("程序退出。")
